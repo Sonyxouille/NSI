e183b5c6772f1d35ee70bdff39c6ddca5e7d1323
Sonyxou <sonyxou@gmail.com>
2026-09-23 09:12:04 +0200

    Début pyxel

--- Pyxel/pyxel_cours.py
+++ Pyxel/pyxel_cours.py
@@ -1,5 +1,21 @@
 import pyxel
 
+class Personnage:
+    def __init__(self) -> None:
+        self.xp = 0
+        self.y = 0
+
+    def movement(self,xp):
+        if pyxel.btn(pyxel.KEY_D):
+            self.xp += 1
+
+    def update(self):
+        self.movement(self)
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.xp, 0, 8, 8, 9)
+
 class App:
     def __init__(self):
         pyxel.init(128, 128, title="Nuit du Code")
@@ -7,6 +23,7 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        Personnage()
         self.x = (self.x + 1) % pyxel.width
 
     def draw(self):
