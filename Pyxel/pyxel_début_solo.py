e536dfe59cda767d40aa75327bc10e7ac820ac72
Sonyxou <sonyxou@gmail.com>
2026-09-25 08:18:27 +0200

    L'oral MP1

--- Pyxel/pyxel_début_solo.py
+++ Pyxel/pyxel_début_solo.py
@@ -1,33 +1,30 @@
 import pyxel
 
-class Personnage:
-    def __init__(self) -> None:
-        self.xp = 0
-        self.y = 0
-
-    def movement(self,xp):
-        if pyxel.btn(pyxel.KEY_D):
-            self.xp += 1
+class Ennemi:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vitesse = 1
 
     def update(self):
-        self.movement(self)
+        self.x = (self.x + self.vitesse) % pyxel.width
 
     def draw(self):
-        pyxel.cls(0)
-        pyxel.rect(self.xp, 0, 8, 8, 9)
+        pyxel.rect(self.x, self.y, 8, 8, 8)
 
-class App:
+class Game:
     def __init__(self):
-        pyxel.init(128, 128, title="Nuit du Code")
-        self.x = 0
+        pyxel.init(160, 120)
+        self.ennemis = [Ennemi(10, 10), Ennemi(50, 30)]
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        Personnage()
-        self.x = (self.x + 1) % pyxel.width
+        for e in self.ennemis:
+            e.update()
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        for e in self.ennemis:
+            e.draw()
 
-App()
+Game()
