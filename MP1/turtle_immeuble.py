cf54713766d98119d1cc89221ad19f0d11891636
Sonyxou <sonyxou@gmail.com>
2026-09-25 09:52:34 +0200

    cloud

--- MP1/turtle_immeuble.py
+++ MP1/turtle_immeuble.py
@@ -270,9 +270,37 @@ def fond():
     goto(1000,1000)
     end_fill()
 
+def cloud():
+    up()
+    h = heading()
+    x = 0
+    y = 280
+    goto(x,y)
+    down()
+    fillcolor('white')
+    begin_fill()
+    goto(x+88,y)
+    angle = 90
+    for i in range(0,4):
+        circle(15,90)
+        angle += 15
+        setheading(angle)
+    angle = 90
+    up()
+    goto(x,y)
+    setheading(180)
+    angle = 90
+    down()
+    for i in range(0,4):
+        circle(-15,90)
+        angle -= 15
+        setheading(angle)
+    end_fill()
+
 down()
 hideturtle()
 speed(100000)
 fond()
+cloud()
 quartier(-300,-100)
 done()
